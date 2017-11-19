0ab3b99e50922693fcf8248a3cc9889460f22902
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-19 11:28:47 -0800

    Add new links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,5 +4,19 @@ class LinksController < ApplicationController
   end
 
   def create
+    @link = Link.create(link_params)
+    if @link.valid?
+      flash[:notice] = "Link added"
+    else
+      flash[:error] = "Invalid URL"
+    end
+
+    redirect_to links_path
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:url)
   end
 end
