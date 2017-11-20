09c923680e532c01bec00e32e11ff0ff8cdd2599
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-19 20:51:32 -0800

    Redirects and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "links#index"
+  root "home#index"
   resources :links, only: [:index, :create, :destroy]
   get "featured", to: "featured_links#index"
   patch "delete_links", to: "users/user_link_deletion_settings#update"
