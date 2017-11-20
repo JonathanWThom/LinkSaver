3444d9abec5a51b19f073ff982f8e1bb9d810e28
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-19 21:30:26 -0800

    Redirect logged in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,13 @@
 class HomeController < ApplicationController
   skip_before_action :require_login
+  before_action :redirect_logged_in_user, if: :current_user
 
   def index
+  end
+
+  private
+
+  def redirect_logged_in_user
+    redirect_to links_path
   end
 end
