ab0a8ff1449616fcd1b68b022da1059ec7387586
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-20 21:35:48 -0800

    Link helper and fix random link bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def random_link_path
-    Link.random.first.url
-  end
 end
---
+++ app/helpers/link_helper.rb
@@ -0,0 +1,9 @@
+module LinkHelper
+  def random_link_path
+    current_user.links.random.first.url
+  end
+
+  def link_count
+    current_user.links.count
+  end
+end
