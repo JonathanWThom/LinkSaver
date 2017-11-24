464e095fbf7fb2958532d699afa15f8d28867e44
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-24 13:26:48 -0800

    Favorite stars

---
+++ app/controllers/favorite_links_controller.rb
@@ -0,0 +1,16 @@
+class FavoriteLinksController < ApplicationController
+  def update
+    @link = Link.find(params[:id])
+
+    if @link.favorite
+      @link.update(favorite: false)
+    else
+      @link.update(favorite: true)
+    end
+
+    respond_to do |format|
+      format.js
+      format.html { redirect_to links_path(current_user) }
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171124184739) do
+ActiveRecord::Schema.define(version: 20171124203718) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20171124184739) do
     t.integer "user_id"
     t.string "title"
     t.text "html_preview"
+    t.boolean "favorite", default: false
     t.index ["created_at"], name: "index_links_on_created_at"
     t.index ["updated_at"], name: "index_links_on_updated_at"
     t.index ["url"], name: "index_links_on_url"
