99f8bcde3f117dd79e865e5a3e8332522a0402dc
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-24 10:44:35 -0800

    Featured links controller spec

---
+++ spec/controllers/featured_links_controller_spec.rb
@@ -0,0 +1,17 @@
+describe LinksController do
+  attr_reader :user
+
+  before(:each) do
+    @user = create(:user)
+    4.times do
+      create(:link, user: user)
+    end
+  end
+
+  describe "GET featured_links#index", type: :request do
+    it "will succeed" do
+      get featured_path(user, as: user)
+      expect(response.status).to eq(200)
+    end
+  end
+end
