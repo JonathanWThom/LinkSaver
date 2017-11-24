861ad734d8285efdbbf86df3e4f662c2fe8725bc
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-24 10:41:25 -0800

    Link controller specs

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,65 +1,74 @@
 describe LinksController do
+  attr_reader :user
+
+  before(:each) do
+    @user = create(:user)
+  end
+
   describe "GET links#index", type: :request do
     it "will succeed" do
-      user = create(:user)
       get links_path(user, as: user)
       expect(response.status).to eq(200)
     end
 
     it "will redirect a non logged in user" do
-      user = create(:user)
       get links_path(user)
       expect(response.status).to eq(302)
     end
 
     it "will succeed with oldest params" do
-      user = create(:user)
       get links_path(user, as: user, oldest: true)
       expect(response.status).to eq(200)
     end
   end
 
   describe "POST links#create", type: :request do
-    it "will succeed with good params" do
+    before(:each) do
       stub_nokogiri
-      user = create(:user)
-      link_attributes = FactoryGirl.attributes_for(:link, user: user)
       get links_path(as: user)
+    end
 
+    it "will succeed with good params" do
       expect {
         post "/links", params: { link: {url: "http://helloworld.com"} }
       }.to change(Link, :count)
-
     end
 
-    # it "will redirect to links path" do
-    #   user = create(:user)
-    #   get links_path(user, as: user, oldest: true)
-    #   expect(response.status).to eq(200)
-    # end
+    it "will fail with bad params" do
+      expect {
+        post "/links", params: { link: {url: ""} }
+      }.to_not change(Link, :count)
+    end
+
+    it "will redirect" do
+      post "/links", params: { link: {url: "http://helloworld.com"} }
+      expect(response.status).to eq(302)
+    end
   end
 
+  describe "GET links#show", type: :request do
+    before(:each) do
+      link = create(:link, user: user)
+      get link_path(link, as: user)
+    end
 
-  # def create
-  #   @link = current_user.links.create(link_params)
-  #   if @link.valid?
-  #     PageContentBuilder.new(@link.id).run
-  #     flash[:notice] = "Link added"
-  #   else
-  #     flash[:error] = "Invalid URL"
-  #   end
-  #
-  #   redirect_to links_path
-  # end
-  #
-  # def show
-  #   @link = Link.find(params[:id])
-  # end
-  #
-  # def destroy
-  #   @link = Link.find(params[:id])
-  #   @link.destroy
-  #
-  #   redirect_to links_path
-  # end
+    it "will succeed with good params" do
+      expect(response.status).to eq(200)
+    end
+  end
+
+  describe "DELETE links#destroy", type: :request do
+    before(:each) do
+      link = create(:link, user: user)
+      delete link_path(link, as: user)
+    end
+
+    it "will delete the link" do
+      expect(Link.count).to eq(0)
+    end
+
+    it "will redirect" do
+      expect(response.status).to eq(302)
+    end
+  end
 end
