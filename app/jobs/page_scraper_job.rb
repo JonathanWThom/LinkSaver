1584c64997e4c780b845c73e3fb7dc165b7a9e87
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-11-24 12:29:08 -0800

    Handle PDFs better

--- app/jobs/page_scraper_job.rb
+++ app/jobs/page_scraper_job.rb
@@ -4,8 +4,34 @@ require "open-uri"
 class PageScraperJob < ApplicationJob
   def perform(link_id)
     link = Link.find(link_id)
-    page = Nokogiri::HTML(open(link.url))
-    html = page.css("body" ).to_html
-    link.update(html_preview: html)
+    @url = link.url
+
+    if File.extname(url) == ".pdf"
+      preview = parse_pdf
+    else
+      preview = parse_html
+    end
+
+    link.update(html_preview: preview)
+  end
+
+  private
+
+  attr_reader :url
+
+  def parse_html
+    page = Nokogiri::HTML(open(url))
+    page.css("body").to_html
+  end
+
+  def parse_pdf
+    io = open(url)
+    reader = PDF::Reader.new(io)
+    text = ""
+    reader.pages.each do |page|
+      text.concat(page.text)
+    end
+
+    text
   end
 end
