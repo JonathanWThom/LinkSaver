bb9a59b82b5e889a41afcf228eb9a319a85cc55f
Jonathan Thom <jonathan.thom1990@gmail.com>
2020-05-22 15:36:24 -0700

    Bring back validation that was removed

--- app/models/link.rb
+++ app/models/link.rb
@@ -7,7 +7,7 @@ class Link < ActiveRecord::Base
   has_many :categories
   has_many :tags, through: :categories
 
-  #validates :url, presence: true, url: true
+  validates :url, presence: true, url: true
 
   scope :newest_first, -> { order(created_at: :desc) }
   scope :oldest_first, -> { order(created_at: :asc) }
