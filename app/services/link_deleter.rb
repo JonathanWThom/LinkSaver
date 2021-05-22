67361d0fd3c18a06852fe4c9109d42cd71a18095
Jonathan Thom <jonathanthom@hey.com>
2021-05-22 09:35:35 -0700

    Remove more unused stuff

--- app/services/link_deleter.rb
+++ app/services/link_deleter.rb
@@ -1,7 +1,4 @@
-# typed: true
-class LinkDeletionWorker
-  include Sidekiq::Worker
-
+class LinkDeleter
   def perform
     User.deletion_on.each do |user|
       user.links.one_month_old.non_favorites.destroy_all
--- lib/tasks/delete_old_links.rake
+++ lib/tasks/delete_old_links.rake
@@ -2,6 +2,6 @@ desc "Delete links older than 1 month"
 
 task :delete_old_links => :environment do
   puts "Deleting old links..."
-  LinkDeletionWorker.new.perform
+  LinkDeleter.new.perform
   puts "Done."
 end
