d16958ba608c04dc0a19bf73f585daf874021748
Jonathan Thom <jonathanthom@hey.com>
2021-08-21 01:04:52 -0700

    Fix syntax

--- app/jobs/page_scraper_job.rb
+++ app/jobs/page_scraper_job.rb
@@ -25,7 +25,7 @@ class PageScraperJob < ApplicationJob
     content = page.read
     page.rewind
     Readability::Document.new(
-      content
+      content,
       tags: %w[p div h1 h2 h3 h4 h5 h6 address article aside footer header main nav section blockquote dd dl dt figcaption figure hr li ol ul pre b strong code data em span img caption col table tbody td tfoot th thead tr label details menu summary]
     ).content
   end
