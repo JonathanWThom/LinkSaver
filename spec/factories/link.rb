45c170443b7329a3479e730a3c97410d03d74b04
Jonathan Thom <jonathanthom@hey.com>
2021-10-04 20:35:35 -0700

    Link title factory that should cause less flakes

--- spec/factories/link.rb
+++ spec/factories/link.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :link do
     user
     address { Faker::Internet.url }
-    page_title { Faker::Beer.name }
+    page_title { Faker::Food.dish }
     html { "<h1>Hello World</h1>" }
   end
 end
